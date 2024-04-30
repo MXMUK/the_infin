27e0d226892d9e8c9b057cbb8f9283f4419075eb
mxmk <romamaksymuk@gmail.com>
2024-04-30 13:10:08 +0200

    mouse events in hero section

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,4 +1,5 @@
-import React, { FC, useRef } from 'react';
+import React, { FC, useEffect, useRef, useState } from 'react';
+import { motion } from 'framer-motion';
 
 import styles from './Header.module.scss';
 
@@ -7,25 +8,86 @@ import ParallaxContainer from '../shared/ParallaxContainer';
 import Image from 'next/image';
 
 import mobileApp from '@/../public/images/mobile-app.png';
+import useMouse from '@react-hook/mouse-position';
+import NavBar from '../NavBar';
 
 const Header: FC = () => {
+  const ref = useRef<HTMLDivElement | null>(null);
+  const mouse = useMouse(ref, {
+    enterDelay: 0,
+    leaveDelay: 0,
+    fps: 15,
+  });
+
+  let mouseXPosition = 0;
+  let mouseYPosition = 0;
+
+  if (mouse.clientX !== null) {
+    mouseXPosition = mouse.clientX;
+  }
+
+  if (mouse.clientY !== null) {
+    mouseYPosition = mouse.clientY;
+  }
+
+  const variants = {
+    default: {
+      x: mouseXPosition,
+      y: mouseYPosition,
+      transition: {
+        type: 'tween',
+      },
+    },
+  };
+
+  const [showCursor, setShowCursor] = useState(true);
+
+  useEffect(() => {
+    const handleMouseLeave = () => {
+      setShowCursor(false);
+    };
+    const handleMouseEnter = () => {
+      setShowCursor(true);
+    };
+
+    if (ref.current) {
+      ref.current.addEventListener('mouseenter', handleMouseEnter);
+    }
+
+    if (ref.current) {
+      ref.current.addEventListener('mouseleave', handleMouseLeave);
+    }
+
+    return () => {
+      if (ref.current) {
+        ref.current.removeEventListener('mouseleave', handleMouseLeave);
+      }
+      if (ref.current) {
+        ref.current.removeEventListener('mouseenter', handleMouseEnter);
+      }
+    };
+  }, [ref]);
+
   return (
-    <header className={styles.header}>
+    <header className={styles.header} ref={ref}>
+      <NavBar />
+
       <div className={styles.header__mobileExample}>
         <div className={styles.header__mobileExample__text}>
           Web + <br /> Mobile app
         </div>
 
-        <ParallaxContainer multiplier={0.5} className={styles.header__mobileExample__image}>
-          <Image src={mobileApp} alt="mobile app" style={{scale: 1.3}} />
+        <ParallaxContainer
+          multiplier={0.5}
+          className={styles.header__mobileExample__image}
+        >
+          <Image src={mobileApp} alt="mobile app" style={{ scale: 1.3 }} />
         </ParallaxContainer>
-
-        {/* <div className={styles.header__mobileExample__image}></div> */}
       </div>
 
       <div className={styles.header__mainBlock}>
         <div className={styles.header__mainBlock__text}>
-          A market-based assessment of your contribution.
+          <div>A market-based assessment of your contribution.</div>
         </div>
 
         <div className={styles.header__mainBlock__titleContainer}>
@@ -38,6 +100,13 @@ const Header: FC = () => {
           </Button>
         </div>
       </div>
+
+      <motion.div
+        style={{ display: showCursor ? 'block' : 'none' }}
+        variants={variants}
+        className={styles.header__cursor}
+        animate="default"
+      />
     </header>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,50 +7,24 @@ import MainSection from './components/Main';
 import NavBar from './components/NavBar';
 import SplashScreen from './components/SplashScreen';
 import { useEffect, useState } from 'react';
-import { motion } from 'framer-motion';
 
 export default function Home() {
   const pathname = usePathname();
   const isHome = pathname === '/';
   const [isLoading, setIsLoading] = useState(isHome);
-  const [mousePosition, setMousePosition] = useState({
-    x: 0,
-    y: 0,
-  });
-
-  useEffect(() => {
-    const mouseMove = (event: MouseEvent): void => {
-      setMousePosition({
-        x: event.clientX,
-        y: event.clientY,
-      });
-    };
-    window.addEventListener('mousemove', mouseMove);
-
-    return () => {
-      window.removeEventListener('mousemove', mouseMove);
-    };
-  }, []);
 
   useEffect(() => {
     if (isLoading) return;
   }, [isLoading]);
 
-  return isLoading && isHome ? (
-  // return false ? (
+  // return isLoading && isHome ? (
+  return false ? (
     <SplashScreen finishLoading={() => setIsLoading(false)} />
   ) : (
     <div>
-      <NavBar />
-
       <Header />
 
       <MainSection />
-
-      <motion.div
-        className="cursor"
-        style={{ x: mousePosition.x, y: mousePosition.y }}
-      />
 
       <Footer />
     </div>
